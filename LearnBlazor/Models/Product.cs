b84da025e8446b20630172837f29cbf2af8e2a85
Bernt Anker <bernt.anker@me.com>
2024-08-21 08:01:33 +0200

    Chapter 12 - Dropdownlist

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -4,5 +4,7 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public double Price { get; set; }
     public bool IsActive { get; set; }
+    public IEnumerable<Product_Prop> ProductProperties { get; set; }
 }
